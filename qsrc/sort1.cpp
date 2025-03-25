6c674d45064084abe1eab3c818be0c3598f0956c
libai <407469531@qq.com>
2025-03-25 17:40:32 +0800

    调整排序结构

--- qsrc/sort1.cpp
+++ qsrc/sort1.cpp
@@ -22,15 +22,22 @@ void quickSort(std::vector<int>& arr, int low, int high){
 }
 /* test 2 ******************冒泡排序*************************/
 void bubbleSort(std::vector<int>& arr){
+    if(arr.size() < 2)
+        return;
     int len = arr.size();
     for(int i=0;i<len;++i){
-        for(int j=i;j<len;++j){
-            if(arr[i] > arr[j]) {
-                swap(arr[i],arr[j]);
+        bool isNoSwap = true;
+        for(int j=0; j<len-i-1; ++j){
+            if(arr[j+1] < arr[j]){
+                isNoSwap = false;
+                swap(arr[j], arr[j+1]);
             }
         }
+        if(isNoSwap)
+            break;
     }
 }
+
 /* test 3 ********************堆排序***********************/
 void heapify(std::vector<int>& arr, int n, int i) {
     int largest = i;
@@ -70,30 +77,69 @@ void insertionSort(std::vector<int>& arr) {
         arr[j+1] = key;
     }
 }
+/* test 5 *******************选择排序************************/
+void selectSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for(int i = 0; i < n; ++i){
+        int min = i;
+        for(int j = i+1; j < n; ++j){
+            if(arr[j] < arr[min])
+                min = j;
+        }
+        swap(arr[min],arr[i]);
+    }
+}
 
 /*********************测试例子构造***********************/
 TEST(sort_test, test1_quickSort){
     std::vector<int> arr = {10, 7, 8, 9, 1, 5};
     int n = arr.size();
+    pVector(arr,"quickSort input");
     quickSort(arr, 0, n - 1);
-    pVector(arr,"quickSort");
+    pVector(arr,"quickSort output");
+
+    vector<int> rightResult = {1,5,7,8,9,10};
+    EXPECT_EQ(arr, rightResult);
 }
 
 TEST(sort_test, test2_bubbleSort){
     std::vector<int> arr = {10, 7, 8, 9, 1, 5};
+    pVector(arr,"bubbleSort input");
     bubbleSort(arr);
-    pVector(arr,"bubbleSort");
+    pVector(arr,"bubbleSort output");
+
+    vector<int> rightResult = {1,5,7,8,9,10};
+    EXPECT_EQ(arr, rightResult);
 }
 
 TEST(sort_test, test3_heapSort){
     std::vector<int> arr = {10, 7, 8, 9, 1, 5};
     int n = arr.size();
+    pVector(arr,"heapSort input");
     heapSort(arr, n);
-    pVector(arr,"heapSort");
+    pVector(arr,"heapSort output");
+
+    vector<int> rightResult = {1,5,7,8,9,10};
+    EXPECT_EQ(arr, rightResult);
 }
 
 TEST(sort_test, test4_insertionSort){
     std::vector<int> arr = {10, 7, 8, 9, 1, 5};
+    pVector(arr,"insertionSort input");
     insertionSort(arr);
-    pVector(arr,"insertionSort");
+    pVector(arr,"insertionSort output");
+
+    vector<int> rightResult = {1,5,7,8,9,10};
+    EXPECT_EQ(arr, rightResult);
+}
+
+TEST(sort_test, test5_selectSort){
+    std::vector<int> arr = {10, 7, 8, 9, 1, 5};
+    pVector(arr,"selectSort input");
+    selectSort(arr);
+    pVector(arr,"selectSort output");
+
+    vector<int> rightResult = {1,5,7,8,9,10};
+    EXPECT_EQ(arr, rightResult);
+
 }
