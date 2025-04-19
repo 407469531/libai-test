1ee1e9d7f4135b47e972e6b136ee23fad02756fa
libai <407469531@qq.com>
2025-04-19 17:07:57 +0800

    vector add case

--- testProject/vector1.cpp
+++ testProject/vector1.cpp
@@ -20,7 +20,10 @@ int trap(vector<int>& height) {
     }
     return ans;
 }
-
+TEST(vector_test, test1_trap){
+    vector<int> vec={0,1,0,2,1,0,1,3,2,1,2,1};
+    EXPECT_EQ(trap(vec), 6);
+}
 /* test 2 ****************** 239.滑动窗空最大值 *************************/
 // 给你一个整数数组nums，有一个大小为k的滑动窗口从数组的最左侧移动到数组的最右侧。
 // 你只可以看到在滑动窗口内的k个数字。滑动窗口每次只向右移动一位。返回滑动窗口中的最大值.
@@ -49,7 +52,13 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k) {
     }
     return ans;
 }
-
+TEST(vector_test, test2_maxSlidingWindow){
+    vector<int> vec={1,3,-1,-3,5,3,6,7};
+    vector<int> res = maxSlidingWindow(vec,3);
+    pVector(res,"maxSlidingWindow");
+    vector<int> rightResult = {3,3,5,5,6,7};
+    EXPECT_EQ(res , rightResult);
+}
 /* test 3 ****************** 1.两数之和 *************************/
 vector<int> twoSum(vector<int>& nums, int target) {
     unordered_map<int,int> value_key;
@@ -61,6 +70,12 @@ vector<int> twoSum(vector<int>& nums, int target) {
         value_key[nums[i]]=i;
     }
     return {};
+}
+TEST(vector_test, test3_twoSum){
+    vector<int> vec={1,2,3,4,5};
+    vector<int> res = twoSum(vec,9);
+    vector<int> rightResult = {3,4};
+    EXPECT_EQ(res , rightResult);
 }
 /* test 4 ****************** 283.移动零 *************************/
 void moveZeroes(vector<int>& nums) {
@@ -75,6 +90,12 @@ void moveZeroes(vector<int>& nums) {
         }
         ++right;
     }
+}
+TEST(vector_test, test4_moveZeroes){
+    vector<int> in = {1,0,3,0,5};
+    moveZeroes(in);
+    vector<int> rightResult = {1,3,5,0,0};
+    EXPECT_EQ(in , rightResult);
 }
 /* test 5 ****************** 15.三树之和 *************************/
 // 判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
@@ -102,6 +123,12 @@ vector<vector<int>> threeSum(vector<int>& nums) {
     }
     return ans;
 }
+TEST(vector_test, test5_threeSum){
+    vector<int> vec={-1,0,1,2,-1,-4};
+    vector<vector<int>> res = threeSum(vec);
+    vector<vector<int>> rightResult = {{-1,-1,2},{-1,0,1}};
+    EXPECT_EQ(res , rightResult);
+}
 /* test 6 ****************** 560.和为k的子数组 *************************/
 // 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
 // 子数组是数组中元素的连续非空序列。
@@ -118,7 +145,15 @@ int subarraySum(vector<int>& nums, int k) {
     }
     return count;
 }
+TEST(vector_test, test6_subarraySum){
+    vector<int> vec1={6,4,3,1};
+    int res1 = subarraySum(vec1,10);
+    EXPECT_EQ(res1, 1);
 
+    vector<int> vec2={1,1,1};
+    int res2 = subarraySum(vec2,2);
+    EXPECT_EQ(res2, 2);
+}
 /* test 7 ****************** 49.字母异位词分组 *************************/
 //给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
 vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -134,55 +169,36 @@ vector<vector<string>> groupAnagrams(vector<string>& strs) {
     }
     return res;
 }
-
-/*********************测试例子构造***********************/
-TEST(vector_test, test1_trap){
-    vector<int> vec={0,1,0,2,1,0,1,3,2,1,2,1};
-    EXPECT_EQ(trap(vec), 6);
-}
-
-TEST(vector_test, test2_maxSlidingWindow){
-    vector<int> vec={1,3,-1,-3,5,3,6,7};
-    vector<int> res = maxSlidingWindow(vec,3);
-    pVector(res,"maxSlidingWindow");
-    vector<int> rightResult = {3,3,5,5,6,7};
-    EXPECT_EQ(res , rightResult);
-}
-
-TEST(vector_test, test3_twoSum){
-    vector<int> vec={1,2,3,4,5};
-    vector<int> res = twoSum(vec,9);
-    vector<int> rightResult = {3,4};
-    EXPECT_EQ(res , rightResult);
-}
-
-TEST(vector_test, test4_moveZeroes){
-    vector<int> in = {1,0,3,0,5};
-    moveZeroes(in);
-    vector<int> rightResult = {1,3,5,0,0};
-    EXPECT_EQ(in , rightResult);
-}
-
-TEST(vector_test, test5_threeSum){
-    vector<int> vec={-1,0,1,2,-1,-4};
-    vector<vector<int>> res = threeSum(vec);
-    vector<vector<int>> rightResult = {{-1,-1,2},{-1,0,1}};
-    EXPECT_EQ(res , rightResult);
-}
-
-TEST(vector_test, test6_subarraySum){
-    vector<int> vec1={6,4,3,1};
-    int res1 = subarraySum(vec1,10);
-    EXPECT_EQ(res1, 1);
-
-    vector<int> vec2={1,1,1};
-    int res2 = subarraySum(vec2,2);
-    EXPECT_EQ(res2, 2);
-}
 TEST(vector_test, test7_groupAnagrams){
     vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
     vector<vector<string>> res = groupAnagrams(strs);
     vector<vector<string>> right = {{"bat"},{"tan","nat"},{"eat","tea","ate"}};
     EXPECT_EQ(res, right);
-
 }
+/* test 8 ****************** 128.最长连续序列 *************************/
+//给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+int longestConsecutive(vector<int>& nums) {
+    unordered_set<int> all;
+    for(auto &n: nums){
+        all.emplace(n);
+    }
+    int maxSequence(0);
+    for(auto &per : all){
+        if(!all.count(per-1)){
+            int perSequence(0);
+            int tmp = per;
+            while(all.count(tmp++)){
+                ++perSequence;
+            }
+            maxSequence = max(maxSequence,perSequence);
+        }
+    }
+    return maxSequence;
+}
+
+TEST(vector_test, test8_longestConsecutive){
+    vector<int> nums = {100,4,200,1,3,2};
+    int res = longestConsecutive(nums);
+    EXPECT_EQ(res, 4);
+}
+
