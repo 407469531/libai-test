f046e5e7fa38c80d219f26293fa891cc470e3605
libai <407469531@qq.com>
2025-04-21 14:53:16 +0800

    add create binary tree function

--- testProject/tree.cpp
+++ testProject/tree.cpp
@@ -1,4 +1,16 @@
 #include "pub.h"
+
+// 创建二叉树
+TreeNode* createBinaryTree(std::vector<int>& nums, int index) {
+    int sz = nums.size();
+    if(index >= sz || nums[index] == -1)
+        return nullptr;
+    TreeNode* root = new TreeNode(nums[index]);
+    root->left = createBinaryTree(nums, 2 * index + 1);
+    root->right = createBinaryTree(nums, 2 * index + 2);
+    return root;
+}
+
 /* test 1 ****************** 94.二叉树的中序遍历 *************************/
 // 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
 void inorder(TreeNode* root, vector<int>& res){
@@ -16,11 +28,10 @@ vector<int> inorderTraversal(TreeNode* root) {
 }
 
 TEST(tree_test, test1_inorderTraversal){
-    TreeNode t3(3);
-    TreeNode t2(2, &t3, nullptr);
-    TreeNode root(1, nullptr, &t2);
+    vector<int> nums={1,-1,2,-1,-1,3,-1};
+    TreeNode *root = createBinaryTree(nums,0);
+    vector<int> res = inorderTraversal(root);
 
-    vector<int> res = inorderTraversal(&root);
     vector<int> right = {1,3,2};
     EXPECT_EQ(res, right);
 }
